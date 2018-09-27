9b3a97800c323663b3401f742c01cdaa00ca5e8e
Arthur Menezes <arthuramsouza@gmail.com>
2018-09-26 21:01:26 -0300

    sched_aperiodic() added

--- sys/kernel/scheduler.c
+++ sys/kernel/scheduler.c
@@ -78,7 +78,7 @@ static void ap_queue_next()
  * tasks to be scheduled, process the delay queue and invoke the real-time scheduler callback.
  * If no RT tasks are ready to be scheduled, invoke the best effort scheduler callback.
  * Update the scheduled task state to running and restore the context of the task.
- * 
+ * sched_rma
  * Delayed tasks are in the delay queue, and are processed in the following way:
  *	- The number of elements (tasks) in queue is counted;
  *	- The a task from the head of the queue is removed and its delay is decremented;
@@ -110,7 +110,7 @@ void dispatch_isr(void *arg)
 		if (krnl_current_task == 0)
 			krnl_current_task = krnl_pcb.sched_be();
 		krnl_task->state = TASK_RUNNING;
-		krnl_pcb.preempt_cswitch++;
+		krnl_pcb.preempt_cswitch++;sched_rma
 #if KERNEL_LOG >= 1
 		dprintf("\n%d %d %d %d %d ", krnl_current_task, krnl_task->period, krnl_task->capacity, krnl_task->deadline, (uint32_t)_read_us());
 #endif
@@ -142,7 +142,7 @@ int32_t sched_rr(void)
 	do {
 		run_queue_next();
 	} while (krnl_task->state == TASK_BLOCKED);
-	krnl_task->bgjobs++;
+	krnl_task->bgjobs++;sched_rma
 
 	return krnl_task->id;
 }
@@ -159,7 +159,7 @@ int32_t sched_rr(void)
  */
 int32_t sched_lottery(void)
 {
-	int32_t r, i = 0;
+	int32_t r, i = 0;sched_rma
 	
 	r = random() % krnl_tasks;
 	if (hf_queue_count(krnl_run_queue) == 0)
@@ -187,7 +187,7 @@ int32_t sched_lottery(void)
  */
 int32_t sched_priorityrr(void)
 {
-	int32_t i, k;
+	int32_t i, k;sched_rma
 	uint8_t highestp = 255;
 	struct tcb_entry *krnl_task2 = NULL;
 	
@@ -283,4 +283,19 @@ int32_t sched_rma(void)
 		krnl_task = &krnl_tcb[0];
 		return 0;
 	}
+}
+
+/**
+ * @brief Aperiodic (AP) scheduler (callback).
+ * 
+ * @return Aperiodic task id.
+ * 
+ */
+
+int32_t sched_aperiodic(void)
+{
+	int32_t k;
+	
+	k = hf_queue_count(krnl_ap_queue);
+	// TODO
 }
