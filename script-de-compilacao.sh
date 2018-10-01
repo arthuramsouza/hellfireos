532070b585ee1aa6115e3866bcc45385736703cf
Gabriell Araujo <ZK9RX6@users.noreply.github.com>
2018-10-01 20:28:14 -0300

    Update script-de-compilacao.sh

--- script-de-compilacao.sh
+++ script-de-compilacao.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
 
+#baixe o gcc 4.6.1 na pasta downloads
+
 #descompactando o gcc 4.6.1
-tar -xzf gcc-4.6.1.tar.gz
+echo "#descompactando o gcc 4.6.1"
+tar -xzf ~/Downloads/gcc-4.6.1.tar.gz
+mv gcc-4.6.1 ~/Downloads/
+echo " "
 
 #exportando o compilador
-export PATH=$PATH:gcc-4.6.1/bin
+echo "#exportando o compilador"
+export PATH=$PATH:~/Downloads/gcc-4.6.1/bin
+echo "$PATH"
+echo " "
 
 #verificando o compilador
+echo "#verificando o compilador"
 mips-elf-gcc -v
+echo " "
 
 #compilando os fontes do simulador do hf-risc
+echo "#compilando os fontes do simulador do hf-risc"
 cd usr/sim/hf_risc_sim
 gcc -O2 hf_risc_sim.c -o hf_risc_sim
 cd ../../../
+echo " "
 
 #compilando a aplicacao
+echo "#compilando a aplicacao"
 cd platform/single_core
 make image
+echo " "
 
 #executando a aplicacao
-../../usr/sim/hf_risc_sim/hf_risc_sim image.bin log.txt
+#echo "#executando a aplicacao"
+#../../usr/sim/hf_risc_sim/hf_risc_sim image.bin log.txt
+#echo " "
