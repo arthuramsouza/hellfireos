4352c8584880d03cb45ffe53482034992019907c
Gabriell Araujo <ZK9RX6@users.noreply.github.com>
2018-10-01 20:57:29 -0300

    Update testando.c

--- app/TESTANDO/testando.c
+++ app/TESTANDO/testando.c
@@ -1,74 +1,88 @@
 #include <hellfire.h>
 
-sem_t s1, s2, s3, s4;
+/*
+ * EXEMPLO DE USO DO READ COUNTER PARA CONTABILIZAR TEMPO:
+ * hellfireos-master/app/task_create_kill/create_kill.c
+ */
 
 void task0(void){
 	int i;
 
+	int x=0;
 	for(;;){
 		for(i = 1; i <= 200; i++)
-			printf("%d ", i);
-		hf_sempost(&s1);
-		for(;;);
+		{
+			x++;
+		}
 	}
 }
 
 void task1(void){
 	int i;
 
+	int x=0;
 	for(;;){
-		hf_semwait(&s1);
-		for(i = 200; i <= 400; i++)
-			printf("%d ", i);
-		hf_sempost(&s2);
-		for(;;);
+		for(i = 1; i <= 200; i++)
+		{
+			x++;
+		}
 	}
 }
 
 void task2(void){
 	int i;
 
+	int x=0;
 	for(;;){
-		hf_semwait(&s2);
-		for(i = 401; i <= 600; i++)
-			printf("%d ", i);
-		hf_sempost(&s3);
-		for(;;);
+		for(i = 1; i <= 200; i++)
+		{
+			x++;
+		}
 	}
 }
 
 void task3(void){
 	int i;
 
+	int x=0;
 	for(;;){
-		hf_semwait(&s3);
-		for(i = 601; i <= 800; i++)
-			printf("%d ", i);
-		hf_sempost(&s4);
-		for(;;);
+		for(i = 1; i <= 200; i++)
+		{
+			x++;
+		}
 	}
 }
 
 void task4(void){
 	int i;
 
+	int x=0;
 	for(;;){
-		hf_semwait(&s4);
-		for(i = 801; i <= 1000; i++)
-			printf("%d ", i);
-		for(;;);
+		for(i = 1; i <= 200; i++)
+		{
+			x++;
+		}
+	}
+}
+
+void task5(void){
+	int i;
+
+	int x=0;
+	for(;;){
+		for(i = 1; i <= 200; i++)
+		{
+			x++;
+		}
 	}
 }
 
 void app_main(void){
-	hf_seminit(&s1, 0);
-	hf_seminit(&s2, 0);
-	hf_seminit(&s3, 0);
-	hf_seminit(&s4, 0);
 
-	hf_spawn(task0, 6, 1, 6, "task a", 1024);
-	hf_spawn(task1, 9, 1, 9, "task b", 1024);
-	hf_spawn(task2, 5, 1, 5, "task c", 1024);
-	hf_spawn(task3, 7, 1, 7, "task d", 1024);
-	hf_spawn(task4, 6, 1, 6, "task e", 1024);
+	hf_spawn(task0, 60, 20, 60, "task a", 1024);
+	hf_spawn(task1, 0, 90, 0, "task b", 1024);
+	hf_spawn(task2, 0, 0, 0, "task c", 1024);
+	hf_spawn(task3, 70, 30, 70, "task d", 1024);
+	hf_spawn(task4, 0, 50, 0, "task e", 1024);
+	hf_spawn(task5, 0, 0, 0, "task f", 1024);
 }
