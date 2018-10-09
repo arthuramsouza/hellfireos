17a1b255f2165a5743cfc8eafde75d001cc91fee
Gabriell Araujo <ZK9RX6@users.noreply.github.com>
2018-10-09 16:02:53 -0300

    Add files via upload

---
+++ script-de-compilacao.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#altere as propriedades deste script para que ele possa ser executado como um programa
+
+#baixe o gcc 4.6.1 na pasta downloads
+
+#descompactando o gcc 4.6.1
+echo "#descompactando o gcc 4.6.1"
+tar -xzf ~/Downloads/gcc-4.6.1.tar.gz
+mv gcc-4.6.1 ~/Downloads/
+echo " "
+
+#exportando o compilador
+echo "#exportando o compilador"
+export PATH=$PATH:~/Downloads/gcc-4.6.1/bin
+echo "$PATH"
+echo " "
+
+#verificando o compilador
+echo "#verificando o compilador"
+mips-elf-gcc -v
+echo " "
+
+#compilando os fontes do simulador do hf-risc
+echo "#compilando os fontes do simulador do hf-risc"
+cd usr/sim/hf_risc_sim
+gcc -O2 hf_risc_sim.c -o hf_risc_sim
+cd ../../../
+echo " "
+
+#compilando a aplicacao
+echo "#compilando a aplicacao"
+cd platform/single_core
+make clean
+make image
+echo " "
+
+#executando a aplicacao
+echo "#executando a aplicacao"
+../../usr/sim/hf_risc_sim/hf_risc_sim image.bin log.txt
+echo " "
