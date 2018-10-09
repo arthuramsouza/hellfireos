fef24b7b63f79079986c49f0730276a9375c6bcd
Gabriell Araujo <ZK9RX6@users.noreply.github.com>
2018-10-09 12:12:05 -0300

    Update scheduler.h

--- sys/include/scheduler.h
+++ sys/include/scheduler.h
@@ -3,3 +3,5 @@ int32_t sched_lottery(void);
 int32_t sched_priorityrr(void);
 int32_t sched_rma(void);
 int32_t sched_aperiodic(void);
+void add_to_delay_queue(delay_node** node_aux);
+void print_jitter();
