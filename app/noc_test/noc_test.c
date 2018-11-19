37d4f4645ba02cbd7d0ea43d9ea571fb7d7d50ee
Gabriell Araujo <ZK9RX6@users.noreply.github.com>
2018-11-19 08:23:11 -0200

    Update noc_test.c

--- app/noc_test/noc_test.c
+++ app/noc_test/noc_test.c
@@ -1,48 +1,176 @@
+//falta apenas inserir as funcoes do jogo
+//a comunicacao ja esta sendo feita
+//so falta colar as funcoes de computar as matrizes
+//as funcoes do jogo devem ser chamadas na hora computar as matrizes
+//deve se inserir as matrizes nos codigos de enviar e receber mensagens
+
 #include <hellfire.h>
 #include <noc.h>
 
-void sender(void)
+#define TRUE 1
+#define FALSE 0
+
+#define MASTER_PORT 5000
+#define SLAVE_PORT 6000
+
+#define MASTER_CPU_ID 0
+
+int number_of_processors;
+int number_of_tasks;
+
+void master(void)
 {
-	int32_t msg = 0;
-	int8_t buf[150];
-	int16_t val;
+	printf("\n");
+	printf("***CPU ID: %d\n", hf_cpuid());
+	printf("\n");
 
-	if (hf_comm_create(hf_selfid(), 1000, 0))
+	int processor;
+	int work=0;
+	int correctness=0;   
+	int until = number_of_processors;
+	int destination;    
+	int must_break = FALSE;    
+	int buffer[1];
+	int value;
+	uint16_t cpu, task, size;
+	int16_t error; 
+
+	if (hf_comm_create(hf_selfid(), MASTER_PORT, 0))
+	{		
 		panic(0xff);
-
-	delay_ms(50);
-
-	while (1){
-		sprintf(buf, "i am cpu %d, thread %d: msg %d size: %d\n", hf_cpuid(), hf_selfid(), msg++, sizeof(buf));
-		val = hf_send(3, 5000, buf, sizeof(buf), 0);
-		if (val)
-			printf("hf_send(): error %d\n", val);
 	}
+
+	//the main processor must send all works
+	while(must_break == FALSE)
+	{
+		//send a work to each processor
+		for(processor=1; processor<number_of_processors; processor++)
+		{             
+			//send the work
+			buffer[0]=work;
+			error = hf_send(processor, SLAVE_PORT, buffer, sizeof(buffer), 0);
+			if (error)
+			{
+				printf("hf_send(): error %d\n", error);
+			}
+
+			//update the next work
+			work++;  
+
+			//the last work was send
+			if(work == number_of_tasks)
+			{
+				must_break = TRUE;
+				until = processor+1;
+				break;
+			}                     
+		}        
+
+		//receive the works back
+		for(processor=1; processor<until; processor++)
+		{	
+			error = hf_recv(&cpu, &task, buffer, &size, 0);
+			if (error)
+			{
+				printf("hf_recv(): error %d\n", error);
+			}
+			else
+			{
+				value = buffer[0];
+
+				printf("VALUE RETURNED: %d\n", value);
+			}
+		}
+	}
+	//terminates the algorithm
+	for(processor=1; processor<number_of_processors; processor++)
+	{
+		//-1 sinalyzes the end work
+		buffer[0]=-1;
+		error = hf_send(processor, SLAVE_PORT, buffer, sizeof(buffer), 0);
+		if (error)
+		{
+			printf("hf_send(): error %d\n", error);
+		}
+	}
+	printf("\n");
+	printf("it's done!!!\n");    
+	printf("\n");
+
+	//just run something
+	int i; while(1){i++;i--;}
 }
 
-void receiver(void)
+void slave(void)
 {
-	int8_t buf[500];
+	printf("\n");
+	printf("***CPU ID: %d\n", hf_cpuid());
+	printf("\n");
+
+	int value;
+	int buffer[1];
 	uint16_t cpu, task, size;
-	int16_t val;
+	int16_t error;
 
-	if (hf_comm_create(hf_selfid(), 5000, 0))
+	if (hf_comm_create(hf_selfid(), SLAVE_PORT, 0))
+	{
 		panic(0xff);
-
-	while (1){
-		val = hf_recv(&cpu, &task, buf, &size, 0);
-		if (val)
-			printf("hf_recv(): error %d\n", val);
-		else
-			printf("%s", buf);
 	}
+
+	while (1)
+	{
+		//receive the work from the master
+		error = hf_recv(&cpu, &task, buffer, &size, 0);
+		if (error)
+		{
+			printf("hf_recv(): error %d\n", error);
+		}
+		else
+		{
+			value = buffer[0];
+
+			printf("VALUE RECEIVED: %d\n", value);
+
+			//-1 sinalyzes the end task
+			if(value==-1)
+			{
+				break;
+			}
+		}
+
+		//compute the work
+
+		//send the work back
+		buffer[0]=value;
+		error = hf_send(MASTER_CPU_ID, MASTER_PORT, buffer, sizeof(buffer), 0);
+		if (error)
+		{
+			printf("hf_send(): error %d\n", error);
+		}
+	}
+
+	printf("\n");
+	printf("it's done!!!\n");
+	printf("\n");
+
+	//just run something
+	int i; while(1){i++;i--;}
 }
 
 void app_main(void)
 {
-	if (hf_cpuid() == 2){
-		hf_spawn(sender, 0, 0, 0, "sender", 4096);
-	}else{
-		hf_spawn(receiver, 0, 0, 0, "receiver", 4096);
+	//important data
+	number_of_processors = hf_ncores();
+	number_of_tasks = 15;
+
+	printf("\n\nNUMBER OF PROCESSORS: %d\n\n",number_of_processors);
+
+	if(hf_cpuid()==MASTER_CPU_ID)
+	{
+		hf_spawn(master, 0, 0, 0, "master", 4096);
+	}
+	else
+	{
+		hf_spawn(slave, 0, 0, 0, "slave", 4096);
 	}
 }
